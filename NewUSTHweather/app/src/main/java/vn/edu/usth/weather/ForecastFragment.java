ef38f7f935dd5a7306c40b6eb5799e0d9c343809
quangminh141005 <minhnq.23bi14288@usth.edu.vn>
2025-09-03 09:46:08 +0700

    change fragment name

--- NewUSTHweather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ NewUSTHweather/app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -10,7 +10,7 @@ import android.widget.Button;
 public class ForecastFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_forecast1, container, false);
+        View view = inflater.inflate(R.layout.fragment_forecast, container, false);
 
         Button button = view.findViewById(R.id.btnGoSecond);
         button.setOnClickListener(v -> {
